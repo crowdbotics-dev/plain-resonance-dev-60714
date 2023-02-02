4d544ff94037f22dfa923c566b41f970a0683b25
Crowdbotics <engineering@crowdbotics.com>
2023-02-02 03:21:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/plain_resonance_dev_60714/MainActivity.java
@@ -0,0 +1,15 @@
+package com.plain_resonance_dev_60714;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "plain_resonance_dev_60714";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "plain-resonance-dev-60714",
+  url: "https://plain-resonance-dev-60714.botics.co",
+  api: "https://plain-resonance-dev-60714.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
